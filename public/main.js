b2e4a73cc7c5852953f481156a426abaef7af9d4
Alessandro Bonardi <Alessandro Bonardi>
2016-02-21 12:49:37 +0000

    Final commit!

--- public/main.js
+++ public/main.js
@@ -71,9 +71,9 @@ $(function() {
     function addParticipantsMessage(data) {
         var message = '';
         if (data.numUsers === 1) {
-            message += "there's 1 participant";
+            message += "There is 1 participant";
         } else {
-            message += "there are " + data.numUsers + " participants";
+            message += "There are " + data.numUsers + " participants";
         }
         log(message);
     }
